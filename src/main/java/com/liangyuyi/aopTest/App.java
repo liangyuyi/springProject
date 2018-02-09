8ce2f403579f6e597716ea0af52d46549503e907
liangyuyi <1241913849@qq.com>
2018-02-09 16:03:58 +0800

    aop未完成

---
+++ src/main/java/com/liangyuyi/aopTest/App.java
@@ -0,0 +1,10 @@
+package com.liangyuyi.aopTest;
+
+/**
+ * Created by liangyuyi on 2018/2/9
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/liangyuyi/aopTest/services/CustomerService.java
@@ -0,0 +1,30 @@
+package com.liangyuyi.aopTest.services;
+
+/**
+ * Created by liangyuyi on 2018/2/9
+ */
+public class CustomerService {
+    private String name;
+    private String url;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void printName() {
+        System.out.println("Customer name : " + this.name);
+    }
+
+    public void printURL() {
+        System.out.println("Customer website : " + this.url);
+    }
+
+    public void printThrowException() {
+        throw new IllegalArgumentException();
+    }
+
+}
